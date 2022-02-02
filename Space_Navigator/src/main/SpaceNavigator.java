28bb4d6af62f1535472020600f1b3adf47ab7065
alex <alex@AlexLaptop>
2022-02-02 16:34:51 -0500

    Basic Window and JavaFX setup -Alex Ephraim

---
+++ Space_Navigator/src/main/SpaceNavigator.java
@@ -0,0 +1,19 @@
+package main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class SpaceNavigator extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		primaryStage.show();
+	}
+	
+
+}
