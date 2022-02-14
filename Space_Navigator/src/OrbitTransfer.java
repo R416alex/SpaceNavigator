343d93da57b9ffc90e7234ca18e443cabaa10dfe
Joseph <josephandersonlax@gmail.com>
2022-02-14 16:11:23 -0500

    OrbitTransfer class will have methods to use for calculations.

---
+++ Space_Navigator/src/OrbitTransfer.java
@@ -0,0 +1,14 @@
+//File: Orbit Transfer algorithm
+//Author: Joseph Anderson
+//Date Modified: 2/14/2022
+
+public class OrbitTransfer {
+        public void HohmanTansfer(double Position1, double Position2, double Time) {
+
+        }
+
+        public void BiEllipticTransfer(double Position1, double Position2, double Time){
+
+        }
+
+        }
