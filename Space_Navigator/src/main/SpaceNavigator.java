9ac493562d4ac03a643397ac59878c70dd3e0d36
alex <alex@AlexLaptop>
2022-02-23 15:15:37 -0500

    Basic Gui Setup Adjustable letterboxing FullScreen Quit Button Background

--- Space_Navigator/src/main/SpaceNavigator.java
+++ Space_Navigator/src/main/SpaceNavigator.java
@@ -1,18 +1,28 @@
 package main;
 
+
+//import com.mathworks.engine.MatlabEngine;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class SpaceNavigator extends Application {
 
+	private GraphicsManager GraphicsManager;
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
-		primaryStage.show();
+		GraphicsManager = new GraphicsManager(primaryStage);
+		
+//		MatlabEngine mateng	 = MatlabEngine.startMatlab();
+//		mateng.eval("syms x;", null, null);
+//		mateng.eval("f = sin(x)/x");
+//		StringWriter output = new StringWriter();
+//		mateng.eval("limit(f,x,0)", output, null);
+//		System.out.println(output.toString());
 	}
 	
 
